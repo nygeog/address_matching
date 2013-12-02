c63f6f169d682a89758de3cf71ba4ab2a99562bf
Daniel M Sheehan <daniel.martin.sheehan@gmailcom>
2013-12-02 17:32:31 -0500

    get rid of . and , and also comments about giving mult. ratios

--- 01_read_csv_compare_diff.py
+++ 01_read_csv_compare_diff.py
@@ -2,7 +2,9 @@ import csv
 import difflib #has Levenshtein distance
 
 #wishlist:
-#1 will want to split out house number at some point
+#1 will want to split out house number at some point so it'll compare
+#  the house number [ratio1] as well as the addess string[ratio2], hmmm, if ends in zip, 
+#  maybe have a ratio3 for that as well. 
 
 datafile = open('data_01.csv', 'r')
 datareader = csv.reader(datafile, delimiter = ',')
@@ -13,9 +15,9 @@ for row in datareader:
 
 # sub 0-9 to be 0 through count of items in list
 for x in range(0, 9):
-	y = data[x][1].lower()
+	y = data[x][1].lower().replace('.','').replace(',','')
 	for compare in range(0, 9):
-		z = data[compare][1].lower()
+		z = data[compare][1].lower().replace('.','').replace(',','')
 		print y + '^' + z
 		print difflib.SequenceMatcher(None, y, z).ratio()
 
