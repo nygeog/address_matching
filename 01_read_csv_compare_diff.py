f33fe2bf67f36e2b4e0b55aa5a4bf5c9c959be7f
nygeog <daniel.martin.sheehan@gmail.com>
2013-12-02 17:26:17 -0500

    changed for loop var name

--- 01_read_csv_compare_diff.py
+++ 01_read_csv_compare_diff.py
@@ -14,8 +14,8 @@ for row in datareader:
 # sub 0-9 to be 0 through count of items in list
 for x in range(0, 9):
 	y = data[x][1].lower()
-	for comp in range(0, 9):
-		z = data[comp][1].lower()
+	for compare in range(0, 9):
+		z = data[compare][1].lower()
 		print y + '^' + z
 		print difflib.SequenceMatcher(None, y, z).ratio()
 
